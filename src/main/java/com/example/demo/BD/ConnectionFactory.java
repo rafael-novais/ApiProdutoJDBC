0dd64c767eae6dc108c42d333037567b0a0a2fc4
rafael <rafael.novais.amorim@gmail.com>
2021-02-04 23:00:14 -0300

    implementando pull de conexões e datasource mchange e c3p0

--- src/main/java/com/example/demo/BD/ConnectionFactory.java
+++ src/main/java/com/example/demo/BD/ConnectionFactory.java
@@ -1,19 +1,30 @@
 package com.example.demo.BD;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 import org.springframework.stereotype.Component;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Component
 public class ConnectionFactory {
+	
+	public DataSource dataSource;
+	
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl(
+					"jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("poli1340");
+		dataSource = comboPooledDataSource;
+	}
+	
     public Connection openConnection() throws SQLException{
-    	Connection connection = DriverManager.getConnection(
-            "jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
-            "root",
-            "poli1340"
-        );
+    	Connection connection = dataSource.getConnection();
     	connection.setAutoCommit(false);
     	return connection;
     }
