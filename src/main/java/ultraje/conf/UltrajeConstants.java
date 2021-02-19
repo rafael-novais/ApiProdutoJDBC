dec6ef9f5a2df765a1ad73561289e5cf6c6d2a28
rafael <rafael.novais.amorim@gmail.com>
2021-02-18 22:29:25 -0300

    removendo chrome ???

--- src/main/java/ultraje/conf/UltrajeConstants.java
+++ src/main/java/ultraje/conf/UltrajeConstants.java
@@ -1,6 +1,7 @@
 package ultraje.conf;
 
 public interface UltrajeConstants {
+	
 	public static final String connection_string = 
 			"jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC";
 	public static final String bd_user = "root";
