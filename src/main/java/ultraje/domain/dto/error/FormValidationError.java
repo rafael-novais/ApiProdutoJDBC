cd5b9fe36efadecb66ce434febe801dbdc611af2
rafael <rafael.novais.amorim@gmail.com>
2021-02-20 09:52:15 -0300

    implementando controller advice

---
+++ src/main/java/ultraje/domain/dto/error/FormValidationError.java
@@ -0,0 +1,25 @@
+package ultraje.domain.dto.error;
+
+public class FormValidationError {
+	private String field;
+	private String message;
+	
+	public FormValidationError(String field, String message) {
+		this.field = field;
+		this.message = message;
+	}
+	public String getField() {
+		return field;
+	}
+	public void setField(String field) {
+		this.field = field;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
--- src/main/java/ultraje/interceptor/ValidationErrorHandler.java
+++ src/main/java/ultraje/interceptor/ValidationErrorHandler.java
@@ -1,8 +1,40 @@
 package ultraje.interceptor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import ultraje.domain.dto.error.FormValidationError;
 
 @RestControllerAdvice
 public class ValidationErrorHandler {
+	
+	@Autowired
+	private MessageSource messageSource;
 
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public List<FormValidationError> formErrorHandle(MethodArgumentNotValidException exception) {
+		
+		List<FormValidationError> errorsDTO = new ArrayList<FormValidationError>();
+		
+		exception.getBindingResult().getFieldErrors()
+			.forEach(fieldError -> {
+				String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+				FormValidationError errorDTO = 
+						new FormValidationError(fieldError.getField(), message);
+				errorsDTO.add(errorDTO);
+			});
+		
+		return errorsDTO;
+	}
+	
 }
