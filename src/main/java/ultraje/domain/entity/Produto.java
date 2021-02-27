bbbc358b032decde6a38b8d68bf90fbef876563b
rafael <rafael.novais.amorim@gmail.com>
2021-02-27 11:57:52 -0300

    configurando lombok

--- src/main/java/ultraje/domain/entity/Produto.java
+++ src/main/java/ultraje/domain/entity/Produto.java
@@ -1,5 +1,8 @@
 package ultraje.domain.entity;
 
+import lombok.Data;
+
+@Data
 public class Produto {
 
 	private int id;
@@ -7,28 +10,4 @@ public class Produto {
     private String descricao;
     private double preco;
     
-	public void setNome(String nome){
-        this.nome = nome;
-    }
-    public void setDescricao(String descricao){
-        this.descricao = descricao;
-    }
-    public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public void setPreco(double preco){
-        this.preco = preco;
-    }
-    public String getNome(){
-        return this.nome;
-    }
-    public String getDescricao(){
-        return this.descricao;
-    }
-    public double getPreco(){
-        return this.preco;
-    }
 }
