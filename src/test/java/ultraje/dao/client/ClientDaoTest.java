e6492991e21791e6617b197bab4bed66e17db096
Rafael Amorim <ramorim@bionexo.com>
2021-05-19 19:51:08 -0300

    comment tests

--- src/test/java/ultraje/dao/client/ClientDaoTest.java
+++ src/test/java/ultraje/dao/client/ClientDaoTest.java
@@ -1,19 +1,16 @@
 package ultraje.dao.client;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import ultraje.domain.entity.Client;
 import ultraje.exception.DAOException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ClientDaoTest {
 
